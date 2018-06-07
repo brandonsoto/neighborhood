609332fbbbf976e963a08be3da0a564a138de96f
Brandon Soto <brandon.soto09@gmail.com>
2018-06-06 23:02:05 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# neighborhood
+
+![Part of the Udacity Front-End Web Development Nanodegree](https://img.shields.io/badge/Udacity-Front--End%20Web%20Developer%20Nanodegree-02b3e4.svg)
+
+> You will develop a single-page application featuring a map of your neighborhood or a neighborhood you would like to visit. You will then add additional functionality to this application, including: map markers to identify popular locations or places you’d like to visit, a search function to easily discover these locations, and a listview to support simple browsing of all locations. You will then research and implement third-party APIs that provide additional information about each of these locations (such as StreetView images, Wikipedia articles, Yelp reviews, etc).
---
+++ js/app.js
@@ -0,0 +1,156 @@
+'use strict';
+
+var initialLocations = [{
+	name: 'Bracher Park',
+	lat: 37.370,
+	long: -122.002
+}, ];
+
+// Declaring global variables now to satisfy strict mode
+var map;
+var clientID;
+var clientSecret;
+
+
+// formatPhone function referenced from
+// http://snipplr.com/view/65672/10-digit-string-to-phone-format/
+function formatPhone(phonenum) {
+    var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (regexObj.test(phonenum)) {
+        var parts = phonenum.match(regexObj);
+        var phone = "";
+        if (parts[1]) { phone += "+1 (" + parts[1] + ") "; }
+        phone += parts[2] + "-" + parts[3];
+        return phone;
+    }
+    else {
+        //invalid phone number
+        return phonenum;
+    }
+}
+
+var Location = function(data) {
+	var self = this;
+	this.name = data.name;
+	this.lat = data.lat;
+	this.long = data.long;
+	this.URL = "";
+	this.street = "";
+	this.city = "";
+	this.phone = "";
+
+	this.visible = ko.observable(true);
+
+	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
+
+	$.getJSON(foursquareURL).done(function(data) {
+		var results = data.response.venues[0];
+		self.URL = results.url;
+		if (typeof self.URL === 'undefined'){
+			self.URL = "";
+		}
+		self.street = results.location.formattedAddress[0];
+     	self.city = results.location.formattedAddress[1];
+      	self.phone = results.contact.phone;
+      	if (typeof self.phone === 'undefined'){
+			self.phone = "";
+		} else {
+			self.phone = formatPhone(self.phone);
+		}
+	}).fail(function() {
+		alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
+	});
+
+	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" +
+        '<div class="content">' + self.phone + "</div></div>";
+
+	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+
+	this.marker = new google.maps.Marker({
+			position: new google.maps.LatLng(data.lat, data.long),
+			map: map,
+			title: data.name
+	});
+
+	this.showMarker = ko.computed(function() {
+		if(this.visible() === true) {
+			this.marker.setMap(map);
+		} else {
+			this.marker.setMap(null);
+		}
+		return true;
+	}, this);
+
+	this.marker.addListener('click', function(){
+		self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" +
+        '<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div></div>";
+
+        self.infoWindow.setContent(self.contentString);
+
+		self.infoWindow.open(map, this);
+
+		self.marker.setAnimation(google.maps.Animation.BOUNCE);
+      	setTimeout(function() {
+      		self.marker.setAnimation(null);
+     	}, 2100);
+	});
+
+	this.bounce = function(place) {
+		google.maps.event.trigger(self.marker, 'click');
+	};
+};
+
+function AppViewModel() {
+	var self = this;
+
+	this.searchTerm = ko.observable("");
+
+	this.locationList = ko.observableArray([]);
+
+	map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 12,
+			center: {lat: 37.370, lng: -122.002}
+	});
+
+	// Foursquare API settings
+	clientID = "";
+	clientSecret = "";
+
+	initialLocations.forEach(function(locationItem){
+		self.locationList.push( new Location(locationItem));
+	});
+
+	this.filteredList = ko.computed( function() {
+		var filter = self.searchTerm().toLowerCase();
+		if (!filter) {
+			self.locationList().forEach(function(locationItem){
+				locationItem.visible(true);
+			});
+			return self.locationList();
+		} else {
+			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+				var string = locationItem.name.toLowerCase();
+				var result = (string.search(filter) >= 0);
+				locationItem.visible(result);
+				return result;
+			});
+		}
+	}, self);
+
+	this.mapElem = document.getElementById('map');
+	this.mapElem.style.height = window.innerHeight - 50;
+}
+
+function startApp() {
+	ko.applyBindings(new AppViewModel());
+}
+
+function errorHandling() {
+	alert("Google Maps has failed to load. Please check your internet connection and try again.");
+}
